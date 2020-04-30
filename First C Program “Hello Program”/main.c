b7eef7ea6b99e0d018fed99201201a889af9548b
Romanykamal <robbenmarkos@gmail.com>
2020-04-30 17:17:26 +0200

    no message

---
+++ First C Program “Hello Program”/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("Hello I am Romany \n");
+    printf("I am a student in Faculty Of Engineering ");
+    return 0;
+}
